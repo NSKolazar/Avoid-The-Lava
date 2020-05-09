f36fb033046c57b200ac0564f8d9692613aa7fcf
NSKolazar <55740088+NSKolazar@users.noreply.github.com>
2020-05-09 22:45:47 +0200

    Player Movement

---
+++ Avoid the lava/Assets/Scripts/Level/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 15f;
+
+    public Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();    
+    }
+    void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal") * Time.fixedDeltaTime * speed;
+
+        rb.MovePosition(rb.position + Vector3.right * x);
+    }
+}

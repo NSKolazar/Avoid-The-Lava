c6db514cfdfbe9052052ccffc720cc30c4f9e2b7
NSKolazar <55740088+NSKolazar@users.noreply.github.com>
2020-05-09 14:49:44 +0200

    Added fonts and audio

---
+++ Avoid the lava/Assets/Scripts/PlayGame.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlazGame : MonoBehaviour
+{
+    public AudioSource effect;
+    public void PlayGame()
+    {
+        effect.Play();
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}

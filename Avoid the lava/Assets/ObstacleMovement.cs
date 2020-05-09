2b993f8a2a80d2bc5df66b847fd1fc76681eb3d5
NSKolazar <55740088+NSKolazar@users.noreply.github.com>
2020-05-10 01:02:55 +0200

    Attempt #1 on making objects spawn to go towards the player

---
+++ Avoid the lava/Assets/ObstacleMovement.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class ObstacleMovement : MonoBehaviour
+{
+    public Transform target;
+    public float speed;
+    void FixedUpdate()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.fixedDeltaTime);
+    }
+}
---
+++ Avoid the lava/Assets/Scripts/Level/BlockSpawner.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class BlockSpawner : MonoBehaviour
+{
+    public Transform[] spawnPoints;
+
+    [SerializeField] public GameObject obstaclePrefab;
+    [SerializeField] public Rigidbody rb;
+
+    [SerializeField] public float timeBetweenWaves = 10f;
+
+    [SerializeField] private float timeToSpawn = 2f;
+    
+    void Update()
+    {
+        if (Time.time >= timeToSpawn)
+        {
+            SpawnObstacles();
+            timeToSpawn = Time.time + timeBetweenWaves;
+        }
+    }
+
+    void SpawnObstacles()
+    {
+        int randomIndex = Random.Range(0,spawnPoints.Length);
+
+        for (int i = 0; i < spawnPoints.Length; i++)
+        {
+            if (randomIndex != i) 
+            {
+                Instantiate(obstaclePrefab, spawnPoints[i].position, Quaternion.identity);
+            }
+        }
+        
+    }
+
+
+}

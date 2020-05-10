f319a30e10181ce20d8c52d92bbfcee4263cb9ef
NSKolazar <55740088+NSKolazar@users.noreply.github.com>
2020-05-10 03:40:57 +0200

    Tested better object movement for obstacles (worked)

---
+++ Avoid the lava/Assets/Scripts/Level/ObjectMove.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class ObjectMove : MonoBehaviour
+{
+
+    public float Speed = 200f;
+
+    void FixedUpdate()
+    {
+        transform.Translate(Vector3.forward * Time.fixedDeltaTime * -Speed, Space.Self);
+    }
+}

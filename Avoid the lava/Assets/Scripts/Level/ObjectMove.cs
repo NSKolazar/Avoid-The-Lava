0c6c7326167043a502442c28efad854e046e5729
NSKolazar <55740088+NSKolazar@users.noreply.github.com>
2020-05-10 15:27:29 +0200

    Movement of obstacles done

--- Avoid the lava/Assets/Scripts/Level/ObjectMove.cs
+++ Avoid the lava/Assets/Scripts/Level/ObjectMove.cs
@@ -4,9 +4,25 @@ public class ObjectMove : MonoBehaviour
 {
 
     public float Speed = 200f;
+    [SerializeField] public GameObject obstacle;
+
 
     void FixedUpdate()
     {
         transform.Translate(Vector3.forward * Time.fixedDeltaTime * -Speed, Space.Self);
+        
+    }
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.CompareTag("End"))
+        {
+            Invoke("Destroy", 0.5f);
+        }
+    }
+
+    void Destroy()
+    {
+        obstacle.SetActive(false);
     }
 }
